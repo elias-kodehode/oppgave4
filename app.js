fcde2a410661aaa918ad05b0e5e59fe98f86edc1
elias-kodehode <elias96.kodehode@gmail.com>
2026-03-19 13:14:41 +0100

    styling

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const app = {};
 
+const benefitsText = [
+    "Ingen tidligere erfaring nødvendig",
+    "Alt materiale inkludert",
+    "For alle aldre",
+    "Slapp av og lad opp batteriene"
+];
 
-function showFeedbackModal(){
-    const feedbackElement = document.getElementById("feedback");
-    console.log(feedbackElement);
-    feedbackElement.style.display = "block";
-}
 document.addEventListener("DOMContentLoaded", () => {
 
+    const textHolder = document.getElementById("text-slide");
+    const p = textHolder.querySelector("p");
+    p.innerText = benefitsText[0];
+    let index = 1;
+
+
+    handleText();
+
+    function handleText(){
+        const timeout = 2000;
+        setTimeout(() => {
+            p.classList.remove("text-animation-out");
+            p.classList.add("text-animation");
+            p.innerText = benefitsText[index % benefitsText.length];
+            index++;
+            setTimeout(() => {
+                p.classList.remove("text-animation");
+                p.classList.add("text-animation-out");
+                handleText();
+            }, timeout);
+        }, timeout);
+    }
+    // setInterval(() => {
+    //     p.innerText = benefitsText[index % benefitsText.length];
+    //     index++;
+    // }, 3000);
 });
+
